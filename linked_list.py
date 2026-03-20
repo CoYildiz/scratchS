6750a7598628e7ee8e563638fdd91801f34ed2b5
co <CoYildiz@github.com>
2026-03-21 01:15:00 +0300

    contains,len,append,pre writed but there is a bug

--- linked_list.py
+++ linked_list.py
@@ -16,9 +16,20 @@ class LinkedList:
     def __repr__(self):
         pass
 
-    
-    def __contains__(self):
-        pass
+    # O(1) best case - O(n) worst case 
+    def __contains__(self, value):
+        if self.head = None:
+            return False
+        last = self.head
+        while last is not None:
+            if last.value == value:
+                return True
+            last = last.next
+        return False
+
+
+
+
     # O(n)
     def __len__(self):
         if self.head == None:
@@ -29,6 +40,7 @@ class LinkedList:
             while last.next:
                 length += 1
             return length
+
     # O(n) 
     def append(self, value):
         if self.head == None:
@@ -39,11 +51,31 @@ class LinkedList:
                 tail = tail.next
             tail.next = Node(value)
 
+    # O(1) i guess
     def prepend(self, value):
-        pass
+        if self.head == None:
+            self.head = Node(value)
+        else:
+            first = Node(value)
+            first.next = self.head
+            self.head = first
 
+    # O(n) i guess worst case 
     def insert(self, value, index):
-        pass
+        if index == 0:
+            self.prepend(value)
+        else:
+            if self.head is None:
+                raise ValueError("Index out of")
+            else:
+                last = self.head
+
+                for i in range(index - 1): # i think there is a bug err:"out of range" fix later
+                    last = last.next
+                new_node = Node(value)
+                new_node.next = last.next
+                last.next = new_node
+
 
     def delete(self, value):
         pass
@@ -56,5 +88,4 @@ class LinkedList:
 
     def print(self):
         pass
-
 
