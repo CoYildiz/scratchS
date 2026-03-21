32cb328c51be9daf8426d94b7556237736198d03
co <CoYildiz@github.com>
2026-03-21 16:47:02 +0300

    some bugs fixed but its not efficient

--- linked_list.py
+++ linked_list.py
@@ -1,4 +1,5 @@
-
+# This linked list is not efficient because it doesn't maintain properties 
+# like length or tail in __init__. Using length and tail would make operations faster.
 
 class Node:
     def __init__(self, value):
@@ -7,18 +8,30 @@ class Node:
 
 
 class LinkedList:
-
+    # add cons later maybe lengt and tail for efficient than implement in the funcs
     def __init__(self):
         self.head = None
 
 
 
     def __repr__(self):
-        pass
+        if self.head is None:
+            return "[]"
+        else:
+            last = self.head
+
+            return_string = f"[{last.value}"
+            
+            while last.next:
+                last = last.next
+                return_string += f", {last.value}"
+            return_string += "]"
+
+            return return_string
 
     # O(1) best case - O(n) worst case 
     def __contains__(self, value):
-        if self.head = None:
+        if self.head == None:
             return False
         last = self.head
         while last is not None:
@@ -37,8 +50,9 @@ class LinkedList:
         else:
             length = 0
             last = self.head
-            while last.next:
+            while last is not None:
                 length += 1
+                last = last.next
             return length
 
     # O(n) 
@@ -70,22 +84,91 @@ class LinkedList:
             else:
                 last = self.head
 
-                for i in range(index - 1): # i think there is a bug err:"out of range" fix later
+                for i in range(index - 1):# i think there is a bug err:"out of range" fix later
+                    if last is None:
+                        raise ValueError("Index out of bounds")
                     last = last.next
                 new_node = Node(value)
                 new_node.next = last.next
                 last.next = new_node
 
-
+    # O(n) 
     def delete(self, value):
-        pass
+        last = self.head
+        if last is not None:
+            if last.value == value:
+                self.head = last.next
+            else:
+                while last.next:
+                    if last.next.value == value:
+                        last.next = last.next.next
+                        return
+                    last = last.next
 
-    def pop(self, index):
-        pass
+    def pop(self, index = -1):
+        if self.head is None:
+            raise ValueError("List is emtpy")
+
+        if index == -1:
+            if self.head.next is None:
+                value = self.head.value
+                self.head = None
+                return value
+            
+            prev = None
+            current = self.head
+
+            while current.next:
+                prev = current
+                current = current.next
+            
+            prev.next = None
+            return current.value
+        # -----------This part was implemented with help from gpt ---------------
+        if index< 0:
+            raise ValueError("Index should be >= 0 or use the func like pop()")
+        
+        if index == 0:
+            value = self.head.value
+            self.head = self.head.next
+            return value
+
+        prev = self.head
+
+        for _ in range(index - 1):
+            if prev.next is None:
+                raise ValueError("Index out of bounds")
+
+            prev = prev.next
+            
+        if prev.next is None:
+            raise ValueError("Index out of bounds")
+            
+        value = prev.next.value
+        prev.next = prev.next.next
+        
+        return value
+    # ----------------------------------------- 
+
 
     def get(self, index):
-        pass
+        if self.head is None:
+            raise ValueError("Index out of bounds")
+        else:
+            last = self.head
+            for i in range(index):
+                if last.next is None:
+                    raise ValueError("Index out of bounds")
+                last = last.next
+            return last.value
 
-    def print(self):
-        pass
 
+
+
+
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.append(20)
+    ll.append(21)
+
+    print(ll)
