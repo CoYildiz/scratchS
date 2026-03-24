65d73f5b8c8c550081375652aab8ee2c6359409d
co <CoYildiz@github.com>
2026-03-24 18:45:04 +0300

    i forgot what func did i write

--- DSaA/linked_list2.py
+++ DSaA/linked_list2.py
@@ -32,11 +32,34 @@ class LinkedList:
 
         
     def __repr__(self) -> str:
-        
-        pass
+       
+        if self._length == 0:
+            return "[]"
+        else:
+            last_node = self.Node(-1)
+            last_node.next = self._head
+            return_string = f"[{last_node.value}"
+
+            while last_node.next:
+                last_node = last_node.next
+                return_string += f",{last_node.value}"
+
+            return_string += "]"
+
+            return return_string
+
+
+
+
+
+
 
     def __contains__(self, value) -> bool:
+
         pass
+
+
+
 
     def __len__(self) -> int:
         pass
@@ -47,9 +70,9 @@ class LinkedList:
 
     def append(self, value) -> None:
         
-        if self._length = 0:
+        if self._length == 0:
             new_node = self.Node(value)
-            self_head = new_node
+            self._head = new_node
             self._tail = new_node
             self._length += 1
         else:
@@ -61,20 +84,60 @@ class LinkedList:
 
 
     def prepend(self, value) -> None:
-        pass
+        new_node = self.Node(value)
+        if self._length == 0:
+            self._head = new_node
+            self._tail = new_node
+        else:
+            new_node.next = self._head
+            self._head = new_node
+        self._length += 1
+
+
     
     def insert(self, index, value):
         pass
 
-    def remove_value(self, value):
-        pass
+
+
+    # if i add a __contains__ func in remove_values i believe is O(n) => O(n^2)
+    # so i dont want to add 
+    # removing unwanted values from LinkedList (its not check value exist or not)
+    def remove_values(self, value) -> None:
+        last_node = self._head
+        if last_node is not None:
+            if last_node.value == value:
+                self._head = last_node.next
+                self._length -= 1
+            else:
+                while last_node.next:
+                    if last_node.next.value == value:
+                        last_node.next = last_node.next.next
+                        self._length -= 1
+                    last_node = last_node.next
+        print(f"Removed {value} value")
 
     def remove_index(self, index):
         pass
 
 
-    def pop(self, index):
+    def pop(self):
         pass
 
     def get(self, index):
         pass
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    ll = LinkedList(1,2)
+    ll.remove_values(2)
+    
